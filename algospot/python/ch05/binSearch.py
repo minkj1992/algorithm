03057fee9e30326bc87f7b4544b4e43923089f06
minkj1992 <37536298+minkj1992@users.noreply.github.com>
2019-04-17 22:42:36 +0900

    이진탐색
    책의 소스코드는 A = []일경우, A=[1]&&x=2일경우 error가 떠야한다. 또한 value not found 구현 x

---
+++ algospot/python/ch05/binSearch.py
@@ -0,0 +1,20 @@
+def binSearch(a, x):
+    """
+    a = non decreasing ordered list
+    x = want to find value
+    return error or index
+    """  
+    lo = 0
+    hi = len(a)
+    # a==[]
+    if not hi: raise ValueError("NULL INDEX INSERTED")
+    while lo < hi:
+        mid = (lo+hi)//2
+        if x < a[mid]: hi = mid
+        else: lo = mid+1
+    return lo-1 if a[lo-1]==x else "VALUE NOT FOUND"
+    
+if __name__=="__main__":
+    li = [79]
+    x = 78
+    print(binSearch(li,x))
