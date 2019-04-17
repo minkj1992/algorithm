f122d11dd607fa71e1a1b1dcde8746260b3e5715
minkj1992 <37536298+minkj1992@users.noreply.github.com>
2019-04-17 23:07:02 +0900

    Update binSearch.py

--- algospot/python/ch05/binSearch.py
+++ algospot/python/ch05/binSearch.py
@@ -13,7 +13,25 @@ def binSearch(a, x):
         if x < a[mid]: hi = mid
         else: lo = mid+1
     return lo-1 if a[lo-1]==x else "VALUE NOT FOUND"
-    
+
+def binSearch2(a, x):
+    """
+    a = non decreasing ordered list
+    x = want to find value
+    return error or index
+    """  
+    lo = -1
+    hi = len(a)
+    # a==[]
+    if not hi: raise ValueError("NULL INDEX INSERTED")
+    while lo+1 < hi:
+        mid = (lo+hi)//2
+        if x > a[mid]: lo = mid
+        else: hi = mid
+    return hi if a[hi]==x else "VALUE NOT FOUND"
+ 
+
+ 
 if __name__=="__main__":
     li = [79]
     x = 78
