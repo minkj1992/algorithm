606df89a83869997a8c4089ac46e6c688ecbdd49
minkj1992 <37536298+minkj1992@users.noreply.github.com>
2019-04-25 14:48:17 +0900

    O(n^2*lgm)
    # Divide and Conquer
    - Error Handling practice
    - identity matrix
    - matrix multiply

---
+++ algospot/python/ch07/squareMatrixPow.py
@@ -0,0 +1,36 @@
+from operator import mul
+class Error(Exception):
+    pass
+class MatrixError(Error):
+    def __init__(self,message):
+        self.message = message
+
+def identity(n):
+    return [[(i==j)*1 for i in range(n)]for j in range(n)]
+# Divide and Conquer   
+def multiply(m1,m2):
+    # ERROR HANDLING
+    if len(m1)!=len(m2) or len(m1[0])!=len(m2[0]):
+        raise MatrixError("NOT SQUARE MATRIX")
+    #MULTIPLY
+    return list(map(
+        lambda row:
+        list(map(
+            lambda *column:
+            sum(map(mul, row, column)),
+            *m2)),
+        m1))
+
+def squareMartixPow(A,m):
+    # BASE CONDITION
+    if m==0: return identity(len(A))
+    # even
+    if m%2==0:half = squareMartixPow(A,m//2)
+    # odd
+    else: return multiply(squareMartixPow(A,m-1),A)
+    return multiply(half,half)
+
+
+if __name__=="__main__":
+    A = [[1,2,3],[1,3],[1,2,3]]
+    print(squareMartixPow(A,3))
