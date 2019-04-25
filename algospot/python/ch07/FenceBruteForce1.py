511982cafee80514879159b2c37559fab07a37c6
minkj1992 <37536298+minkj1992@users.noreply.github.com>
2019-04-25 18:00:26 +0900

    O(n^2)

--- algospot/python/ch07/FenceBruteForce1.py
+++ algospot/python/ch07/FenceBruteForce1.py
@@ -1,3 +1,4 @@
+# O(n^2)
 # 울타리 문제,두번째 for문 indexing
 def FenceBruteForce(rect):
     ret = 1
@@ -6,7 +7,20 @@ def FenceBruteForce(rect):
         for r in range(l+1,len(rect)+1):
             ret= max(ret,(r-l)*min(rect[l:r]))
     return ret
+# 종만 행님코드,indexing이 훨씬 깔끔하다. [:]사용하지 않고 minheight 만 그때그때 구해준다.
+def FenceBruteForce2(rect):
+    ret = 1
+    for l in range(len(rect)):
+        minHeight = rect[l]
+        # 파이썬에서 [:outofBound]여도 error 안뜬다.
+        for r in range(l,len(rect)):
+            minHeight = min(minHeight,rect[r])
+            ret= max(ret,(r-l+1)*minHeight)
+    return ret
+
 for _ in range(int(input())):
     input()
     rect = [int(i) for i in input().split()]
     print(FenceBruteForce(rect))
+
+ 
