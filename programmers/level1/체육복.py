7636efce013e45cbde8c3b0e9f54ead70e9b6009
minkj1992 <minkj1992@gmail.com>
2019-07-04 16:07:54 +0900

    전화번호목록 3rd try

--- programmers/level1/체육복.py
+++ programmers/level1/체육복.py
@@ -35,20 +35,18 @@ def solution(n, lost, reserve):
                 break
     return n-len(lost)
 
-
-# # 만약 lost로 for 문을 돈다면
-
-# def solution(n, lost, reserve):
-#     lost.sort();reserve.sort()
-#     num = len(reserve);
-#     for i in range(len(lost)):
-#         # 현재 lost + 1이 lost에 존재하면서, reserve에 존재한다면 우선순위가 현재가 아닌 뒤에 가 있게 된다.
-#         for j in [0,-1,1]:
-#             if lost[i]+j in reserve:
-#                 if lost[i+1] not in reserve:
-#                     # del lost[i]
-#                     # reserve를 지워주어야 한다.
-#                     reserve.remove(lost[i])
-#                 break
-#     return n-len(lost)+min(len(lost),(num-len(reserve)))
+# 만약 lost로 for 문을 돈다면
+def solution(n, lost, reserve):
+    lost.sort();reserve.sort()
+    num = len(reserve);
+    for i in range(len(lost)):
+        # 현재 lost + 1이 lost에 존재하면서, reserve에 존재한다면 우선순위가 현재가 아닌 뒤에 가 있게 된다.
+        for j in [0,-1,1]:
+            if lost[i]+j in reserve:
+                if lost[i+1] not in reserve:
+                    # del lost[i]
+                    # reserve를 지워주어야 한다.
+                    reserve.remove(lost[i])
+                break
+    return n-len(lost)+min(len(lost),(num-len(reserve)))
 
